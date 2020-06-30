e3a177c79e51dfc8d215d1d288ec8367c7656671
Jaclyn Horton <jaclynchorton@gmail.com>
2020-06-30 11:42:16 -0700

    Added detail.html, updated models.py, views.py, home.html, urls.py

--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,11 @@
 from django.db import models
 
 class Job(models.Model):
+    """ Model for the job image and summary fields in Admin """
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        """ Provides a description of each job summary in Admin """
+        return self.summary
 
